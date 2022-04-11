e5c8a4d67987f770bc8b2b1a17e34a44be0ec0b7
mJeylanii <95252871+mJeylanii@users.noreply.github.com>
2022-04-11 13:34:44 +0300

    Add files via upload

---
+++ UsHesap/src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Sayi giriniz : ");
+        int y = inp.nextInt();
+        for (int i = 0; i <= 10; i++)
+        {
+            System.out.println("Sayi : " + Math.pow(2, i));
+        }
+
+    }
+
+}
