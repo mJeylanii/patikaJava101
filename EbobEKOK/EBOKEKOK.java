bdf11d3a0111014c398135b187b6fdb96e0e3fee
Mohamed Jeylani <95252871+mJeylanii@users.noreply.github.com>
2022-04-24 13:47:36 +0300

    Sum

---
+++ EbobEKOK/EBOKEKOK.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int say1, say2, min, max, ebob = 0, ekok = 0;
+        Scanner giris = new Scanner(System.in);
+        System.out.print("Birinci sayıyı giriniz : ");
+        sayi1 = giris.nextInt();
+        System.out.print("İkinci sayıyı giriniz : ");
+        sayi2 = giris.nextInt();
+
+        min = (sayi1 > sayi2) ? sayi2 : sayi1;
+        max = (sayi1 > sayi2) ? sayi1 : sayi2;
+
+        //Ebok bulmak icin while dongusu
+        while (true) {
+            if (sayi1 % min == 0 && sayi2 % min == 0) {
+                ebob = min;
+                break;
+            } else {
+                min -= 1;
+            }
+        }
+        //Ekok bulmak icin while dongusu
+        while (true) {
+            if (max % sayi1 == 0 && max % sayi2 == 0) {
+                ekok = max;
+                break;
+            } else {
+                max += 1;
+            }
+        }
+        
+        System.out.printf("Ekok : %d\nEbob : %d", ekok, ebob);
+        giris.close();
+    }
+}
